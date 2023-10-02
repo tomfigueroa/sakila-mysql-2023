d8a90ea117f5b8fffbee70096612f9005d93ca7c
tomfigueroa Alberto <t.figueroa@javeriana.edu.co>
2023-10-02 20:06:53 +0000

    fin ejercicio 2oct

--- scripts/Ejercicio2Oct.sql
+++ scripts/Ejercicio2Oct.sql
@@ -4,11 +4,15 @@
 
 with datos_pagos as (
     select
+        store.store_id,
         MONTH(payment_date) as mes,
         YEAR(payment_date) as anno,
         SUM(amount) as amount
         from payment
+            join staff using(staff_id)
+            join store on staff.store_id = store.store_id
         group by 
+        store.store_id,
         MONTH(payment_date),
         YEAR(payment_date)
 ),
@@ -27,6 +31,7 @@ datos_tiendas as (
 
 datos_alquiler as (
     select
+        store_id,
         store,
         MONTH(rental_date) as mes,
         YEAR(rental_date) as anno,
@@ -34,7 +39,7 @@ datos_alquiler as (
     from rental
         join datos_tiendas using (inventory_id)
     group by 
-        store,
+        store_id,
         MONTH(rental_date),
         YEAR(rental_date)
 ),
@@ -42,19 +47,51 @@ datos_alquiler as (
 datos_alquiler_y_pagos as(
     select
         store,
+        store_id,
         mes,
         anno,
+        (amount/qty) as prom,
         qty,
         amount
         from datos_alquiler
-        join datos_pagos using (mes,anno)
+        join datos_pagos using (store_id,mes,anno)
+        group by 
+        store_id,
+        mes,
+        anno
+),
+
+datos_por_columnas as(
+    select
+        store,
+        sum(case when anno=2005 and mes=5 then prom else 0 end) as may2005,
+        sum(case when anno=2005 and mes=6 then prom else 0 end) as jun2005,
+        sum(case when anno=2005 and mes=7 then prom else 0 end) as jul2005
+
+    from datos_alquiler_y_pagos
+    group by store
+), 
+
+calculos as(
+    select 
+    store,
+    may2005,
+    jun2005,
+    (jun2005-may2005) as diffmes1,
+    ((jun2005 - may2005)/may2005)*100 as 'varmes1%',
+    jul2005,
+    (jul2005 - jun2005) as diffmes2,
+    ((jul2005 - jun2005)/jun2005)*100 as 'varmes2%'
+    from datos_por_columnas
 )
 
 
 
 
 
-select *
-    from  datos_alquiler_y_pagos
 
-    limit 20;
+select *
+    from  calculos
+
+
+    limit 40;
